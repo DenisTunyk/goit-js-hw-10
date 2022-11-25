8e11c6339fd2c16fdf8a8d83f9fc6b007d648029
DenisTunyk <tundenis43@gmail.com>
2022-11-25 22:02:32 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const ref = {
     input: document.querySelector("#search-box"),
+    list: document.querySelector(".country-list"),
+    info: document.querySelector(".country-info"),
 }
 
 ref.input.addEventListener("input", debounce(onFormInput, 300));
@@ -25,12 +27,37 @@ function onFormInput() {
             return;
         }
         if ((data.length >= 2) && (data.length <= 10)) {
-            
+            console.log(data);
+            //ref.list.insertAdjacentHTML('beforeend', createMarkupList(data));
+            ref.info.innerHTML = "";
+            ref.list.innerHTML = createMarkupList(data);
         }
-        if (data.length == 1) {
-            
+        if (data.length === 1) {
+            ref.list.innerHTML = "";
+            ref.info.innerHTML = createMarkupInfo(data);
         }
         console.log(data.length)
-    }).catch(Notify.failure("Oops, there is no country with that name"));
+    }).catch(err => {
+        Notify.failure("Oops, there is no country with that name");
+        console.log(err);
+    });
     //console.log(arr.length);
+}
+
+function createMarkupList(arr) {
+    return arr.map(({flags, name}) => `<li><img src="${flags.svg}" alt="${name.common}"><h3>${name.common}</h3></li>`)
+}
+
+function createMarkupInfo(arr) {
+    //const values = Object.values(arr.languages);
+    //console.log(arr.languages);
+    return arr.map(({ flags, name, capital, population, languages }) => `
+      <div class="country">
+        <img src="${flags.svg}" alt="${name.common}" width="80"/>
+        <h2>${name.common}</h2>
+      </div>
+      <div><b>Capital: </b>${capital}</div>
+      <div><b>Population: </b>${population}</div>
+      <div><b>Languages: </b>${Object.values(languages).map(item => ` ${item}`)}</div>
+      `);
 }
